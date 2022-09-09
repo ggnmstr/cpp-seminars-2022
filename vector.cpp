30eb1182fe2e2763a876942a238bb8f509492e79
Artemiy Sartakov <artfly94@gmail.com>
2022-09-09 21:34:34 +0700

    added c/cpp vector interfaces

---
+++ vector.cpp
@@ -0,0 +1,38 @@
+class vector {
+
+int * data;
+int size;
+int capacity;
+
+vector(int capacity) {
+    this->capacity = capacity;
+    this->size = size;
+    this->data = new int[capacity]();
+
+}
+
+~vector() {
+    delete[] data;
+}
+
+// adds element to the last avaialable position,
+// resizes vector if current capacity is reached
+void push_back(int num);
+// resizes vector to given capacity
+void resize(int new_capacity);
+
+int size() {
+    return this->size;
+}
+
+void set(int num, int idx);
+int at(int idx);
+};
+
+int main(int argc, char ** argv) {
+    // vector v;
+    // v.size();
+    // // size(&v)
+    // vector v1;
+    // v1.size();
+}
