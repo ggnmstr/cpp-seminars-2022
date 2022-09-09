b38021fb89c087ccefc3f225af5bbd10416733cc
Artemiy Sartakov <artfly94@gmail.com>
2022-09-09 21:52:56 +0700

    reformat

--- vector.cpp
+++ vector.cpp
@@ -1,38 +1,32 @@
 class vector {
+  int *data;
+  int size;
+  int capacity;
 
-int * data;
-int size;
-int capacity;
-
-vector(int capacity) {
+  vector(int capacity) {
     this->capacity = capacity;
     this->size = size;
     this->data = new int[capacity]();
+  }
 
-}
+  ~vector() { delete[] data; }
 
-~vector() {
-    delete[] data;
-}
+  // adds element to the last avaialable position,
+  // resizes vector if current capacity is reached
+  void push_back(int num);
+  // resizes vector to given capacity
+  void resize(int new_capacity);
 
-// adds element to the last avaialable position,
-// resizes vector if current capacity is reached
-void push_back(int num);
-// resizes vector to given capacity
-void resize(int new_capacity);
+  int size() { return this->size; }
 
-int size() {
-    return this->size;
-}
-
-void set(int num, int idx);
-int at(int idx);
+  void set(int num, int idx);
+  int at(int idx);
 };
 
-int main(int argc, char ** argv) {
-    // vector v;
-    // v.size();
-    // // size(&v)
-    // vector v1;
-    // v1.size();
+int main(int argc, char **argv) {
+  // vector v;
+  // v.size();
+  // // size(&v)
+  // vector v1;
+  // v1.size();
 }
